bdbca0f1fd3dc603114d1614ce3768cc5b68b69d
SteveKhuu <Stephen_Khuu@epam.com>
2012-11-14 16:44:13 -0500

    refactored calendar generation added location to ics files

--- eventPlanner/views.py
+++ eventPlanner/views.py
@@ -34,7 +34,7 @@ def update(request, event_id):
 def attend(request, event_id):
   return HttpResponse("You're attending event %s." % event_id)
 
-def export(request, event_id):
+def make_calendar_object(event_id):
   event = get_object_or_404(Events, pk=event_id)
 
   site = Site.objects.get_current()
@@ -49,6 +49,7 @@ def export(request, event_id):
 
   eventObj = Event()
   eventObj.add('summary', event.name)
+  eventObj.add('location', event.location)
   eventObj.add('dtstart', event.start_datetime)
   eventObj.add('dtend', event.end_datetime)
   eventObj.add('dtstamp', event.created_datetime)
@@ -61,45 +62,24 @@ def export(request, event_id):
   for line in cal.content_lines():
     if line:
       output += line + "\n"
+  
+  return output
 
+def export(request, event_id):
+  event = get_object_or_404(Events, pk=event_id)
+  output = make_calendar_object(event_id)
+  
   response = HttpResponse(output, mimetype="text/calendar")
   response['Content-Disposition'] = 'attachment; filename=%s.ics' % slugify(event.name + "-" + str(event.start_datetime.year))
   return response
 
 def send_email(request, event_id):
   event = get_object_or_404(Events, pk=event_id)
-
-  site = Site.objects.get_current()
-
-  site_token = site.domain.split('.')
-  site_token.reverse()
-  site_token = '.'.join(site_token)
+  output = make_calendar_object(event_id)
   
-  cal = Calendar()
-  cal.add('prodid', '-//%s Events Calendar//%s//' % (site.name, site.domain))
-  cal.add('version', '2.0')
-
-  eventObj = Event()
-  eventObj.add('summary', event.name)
-  eventObj.add('dtstart', event.start_datetime)
-  eventObj.add('dtend', event.end_datetime)
-  eventObj.add('dtstamp', event.created_datetime)
-  eventObj['uid'] = '%dT%d.events.%s' % (event.id, random.randrange(111111111,999999999), site_token)
-  eventObj.add('priority', 5)
-
-  cal.add_component(eventObj)
-
-  output = ""
-  for line in cal.content_lines():
-    if line:
-      output += line + "\n"
-
-#  send_mail(event.name, event.description, 'Stephen_Khuu@epam.com',
-#    ['Stephen_Khuu@epam.com', 'g7khuust@gmail.com'], fail_silently=False)
-#
   attachment_name = '%s.ics' % slugify(event.name + "-" + str(event.start_datetime.year))
   
-  mail = EmailMessage(event.name, event.description, 'no_reply_not_even_you_Os@epam.com', ['Tom_Klimovski@epam.com', 'Osman_Ishaq@epam.com', 'Frank_Vanderzwet@epam.com', 'Stephen_Khuu@epam.com'])
+  mail = EmailMessage(event.name, event.description, 'Stephen_Khuu@epam.com', ['Tom_Klimovski@epam.com', 'Osman_Ishaq@epam.com', 'Frank_Vanderzwet@epam.com', 'Stephen_Khuu@epam.com'])
   mail.attach(attachment_name, output, 'text/calendar')
   mail.send()
   
