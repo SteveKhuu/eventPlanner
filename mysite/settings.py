20ae92286f282ac06f8b25a5121af08c54492a1b
SteveKhuu <Stephen_Khuu@epam.com>
2012-11-15 11:41:56 -0500

    moved templates outside of apps added base template

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/assets/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -65,7 +65,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    PROJECT_PATH + '/templates/'
+    PROJECT_PATH + '/../templates/'
 )
 
 INSTALLED_APPS = (
@@ -160,6 +160,6 @@ LOGGING = {
     }
 }
 
-# For Heroku deployment
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#Uncomment For Heroku deployment
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
